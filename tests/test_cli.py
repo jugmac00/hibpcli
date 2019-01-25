fe09fd4c9c5bb92fe9582bf4fff5495e429e2a4b
Jürgen Gmach <juergen.gmach@apis.de>
2019-01-25 23:36:53 +0100

    Test the "no leaks" case
    modified:   tests/test_cli.py

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,4 +1,5 @@
 from click.testing import CliRunner
+from mock import patch
 
 from hibpcli.cli import main
 
@@ -10,5 +11,17 @@ def test_keepass_subcommand_returns_leaked_entry():
 Please enter the master password for the database: 
 The passwords of following entries are leaked:
 [b'Entry: "test_title (test_user)"']
+"""
+    assert result.output == expected_output
+
+
+@patch("hibpcli.cli.check_passwords_from_db")
+def test_keepass_subcommand_returns_all_ok(mock_check):
+    mock_check.return_value = list()
+    runner = CliRunner()
+    result = runner.invoke(main, ["keepass"], input="\n".join(["tests/test.kdbx", "test"]))
+    expected_output = """Please enter the path to the database: tests/test.kdbx
+Please enter the master password for the database: 
+Hooray, everything is safe!
 """
     assert result.output == expected_output

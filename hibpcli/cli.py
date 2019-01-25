9a097561cb9e00f72d9fcc0c89ccfd5d50988ded
Jürgen Gmach <juergen.gmach@apis.de>
2019-01-25 22:59:10 +0100

    Move keepass check into subcommand
    In order to prepare for the new interface #17, the keepass check
    was moved into its own subcommand.
    
    modified:   README.md
    modified:   hibpcli/cli.py
    modified:   tests/test_cli.py

--- hibpcli/cli.py
+++ hibpcli/cli.py
@@ -3,9 +3,14 @@ import click
 from hibpcli.keepass import check_passwords_from_db
 
 
-@click.command()
+@click.group()
 def main():
     """Command line interface to the haveibeenpwned.com API."""
+    pass
+
+@click.command()
+def keepass():
+    """Check all passwords stored in the keepass database."""
     path = click.prompt("Please enter the path to the database", type=click.Path(exists=True))
     master_password = click.prompt("Please enter the master password for the database", hide_input=True)
     # needs error handling
@@ -15,6 +20,9 @@ def main():
         click.echo(rv)
     else:
         click.echo("Hooray, everything is safe!")
+
+
+main.add_command(keepass)
 
 
 if __name__ == "__main__":

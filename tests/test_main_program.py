5c0d1acdc5346e06a30d59a03ba27504cff297dc
Jürgen Gmach <juergen.gmach@apis.de>
2019-01-26 10:45:42 +0100

    Fix typos
    renamed:    tests/test_main_programm.py -> tests/test_main_program.py

--- tests/test_main_program.py
+++ tests/test_main_program.py
@@ -1,7 +1,7 @@
 import os
 
 
-def test_usage_of_module_as_main_programm():
+def test_usage_of_module_as_main_program():
     """Make sure module can be executed.
 
     More info about this interesting way of testing:

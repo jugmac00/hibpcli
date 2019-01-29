6dff14c7651d6fbe6f37c097a3d777735179dc5c
Jürgen Gmach <juergen.gmach@apis.de>
2019-01-29 22:41:27 +0100

    Do not call real hipb API in tests
    cf #15
    
    modified:   tests/test_password.py

--- tests/test_password.py
+++ tests/test_password.py
@@ -1,14 +1,21 @@
 import pytest
 
+from mock import patch
+
 from hibpcli.password import Password
+
+# this is just a small part of a real response
+RESPONSE_TEXT = "FE5CCB19BA61C4C0873D391E987982FBBD3:74831\r\nFF36DC7D3284A39991ADA90CAF20D1E3C0D:1\r\nFFF983A91443AE72BD98E59ADAB93B31974:2"
 
 
 def test_password_signature():
     with pytest.raises(TypeError):
         Password()  # pylint: disable=E1120
 
-def test_is_password_leaked():
-    # todo: until now, the test tests against the live API
-    # todo: mock requests
+
+@patch("hibpcli.password.requests.get")
+def test_password_is_password_leaked(mock_get):
+    mock_get.return_value.text = RESPONSE_TEXT
     p = Password("test")
     assert p.is_password_leaked() is True
+    mock_get.assert_called_with("https://api.pwnedpasswords.com/range/A94A8")

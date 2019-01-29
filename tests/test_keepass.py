4489aeba09eb1bc1138ddc3e0a6299818eb23f8b
Jürgen Gmach <juergen.gmach@apis.de>
2019-01-29 23:36:08 +0100

    Do not test is_leaked twice
    modified:   hibpcli/keepass.py
    modified:   tests/test_keepass.py

--- tests/test_keepass.py
+++ tests/test_keepass.py
@@ -1,8 +1,12 @@
+from mock import patch
+
 from hibpcli.keepass import check_passwords_from_db
 
 
-def test_check_passwords_from_db():
+@patch("hibpcli.keepass.Password.is_leaked")
+def test_check_passwords_from_db(mock_is_leaked):
     path = 'tests/test.kdbx'
     password = 'test'
-    assert str(check_passwords_from_db(path, password)) == '''[b'Entry: "test_title (test_user)"']'''
-    
+    mock_is_leaked.return_value = lambda x: x == "test"
+    expected_output = '''[b'Entry: "test_title (test_user)"']'''
+    assert str(check_passwords_from_db(path, password)) == expected_output

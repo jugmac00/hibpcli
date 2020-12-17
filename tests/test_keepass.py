316c2618f38b7c227680da479d8d054fceab7369
Jürgen Gmach <juergen.gmach@googlemail.com>
2020-12-17 19:19:16 +0100

    apply pre-commit autoupdate
    - fix typing issue
    
    modified:   .pre-commit-config.yaml
    modified:   tests/test_keepass.py

--- tests/test_keepass.py
+++ tests/test_keepass.py
@@ -6,7 +6,7 @@ from hibpcli.keepass import check_passwords_from_db
 @patch("hibpcli.keepass.Password.is_leaked")
 def test_check_passwords_from_db(mock_is_leaked):
     path = "tests/test.kdbx"
-    password = "test"
+    password = "test"  # nosec
     mock_is_leaked.return_value = lambda x: x == "test"
     expected_output = """[Entry: "test_title (test_user)"]"""
     assert str(check_passwords_from_db(path, password)) == expected_output

0b9ad95aa4f92b144f9b5130b56d438d3ab7dbb0
Jürgen Gmach <juergen.gmach@canonical.com>
2026-05-22 17:36:35 +0200

    Fix test_check_passwords_from_db
    The test was always returning true, now the test actually works.
    
    Thanks to @transfairs
    
    This fixes #133

--- tests/test_keepass.py
+++ tests/test_keepass.py
@@ -7,6 +7,6 @@ from hibpcli.keepass import check_passwords_from_db
 def test_check_passwords_from_db(mock_is_leaked):
     path = "tests/test.kdbx"
     password = "test"  # nosec
-    mock_is_leaked.return_value = lambda x: x == "test"
+    mock_is_leaked.side_effect = lambda x: x == "test"
     expected_output = """[Entry: "test_title (test_user)"]"""
     assert str(check_passwords_from_db(path, password)) == expected_output
